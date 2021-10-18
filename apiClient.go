1d18dbe5ca556c894bfe27d89a9daf18c62d565d
StrRoma <gotbitbots@gmail.com>
2021-10-18 21:11:14 +0300

    new apiclient

--- apiClient.go
+++ apiClient.go
@@ -1,6 +1,34 @@
 package apiclient
 
-import "io"
+import (
+	"fmt"
+)
+
+type DebugStr struct {
+	LastRequestMethod string
+	LastResponseData  []byte
+	LastSentData      []byte
+}
+
+func (D *DebugStr) FormatError() string {
+	return fmt.Sprintf("method: %s, rd: %s, sd: %s",
+		D.LastRequestMethod,
+		D.LastResponseData,
+		D.LastSentData,
+	)
+}
+
+type ExchangeAPI struct {
+	ApiKey    string
+	ApiSecret string
+	AccountID string
+
+	Root string
+
+	Depth int
+
+	DebugStr
+}
 
 //APIClient interface for all excahnge api
 type APIClient interface {
@@ -9,52 +37,18 @@ type APIClient interface {
 	 * вызове ф-ии он остается пустым.
 	 * logger надо вызвать log.SetOutput(logger), чтобы все log.* функции писали куда надо
 	 */
-	Init(accountID string, apiKey string, apiSecret string) error
+	Init(apiKey, apiSecret, accountID string, depth int) error
 
 	/* Получает список пар и балансы валют, переводит в формат apiclient и заполняет
-	 * соотв. поля реализации. Возвращает указатель на таблицу балансов реализации.
-	 *
-	 * Функция должна быть вызвана первой, до вызова любых функций интерфейса, кроме
-	 * функций-свойств (Proc_XX). Затем ф-ия вызывается после каждого выполненного
-	 * ордера для проверки балансов. Следует избегать блокировок.
+	 * соотв. поля реализации. Возвращает карту балансов реализации и ошибку.
 	 */
-	GetBalances() (*map[string]Balance, error)
-
-	/* Получает список всех открытх сделок на бирже(не только наших), заполняет структуру в формате apiclient
-	 * и возвращает указтель на эту структуру. Ф-ия вызывается часто, перед каждой
-	 * сделкой для анализа orderBook. Следует избегать блокировок.
-	 *
-	 * arguments:
-	 *   symbol   text-id пары в формате apiclient
-	 * 		(например "BTC_ETH" слева валюта основная, справа валюта которую покупаем при вызове buy)
-	 * 		в вид который принимает биржа, необходимо привести самому в этой функции
-	 *
-	 * должен отсортировать asks по возрастанию цены
-	 * bids по убыванию цены
-	 */
-	GetOrderBook(symbol string) (*OrderBook, error)
-
-	/* Ставит ордер на продажу.
-	 * теперь не вызывает getOrderInfo
-	 *
-	 * arguments:
-	 *   symbol   text-id пары в формате apiclient
-	 * 		(например "BTC_ETH" слева валюта основная, справа валюта которую покупаем при вызове buy)
-	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
-	 *   amount   кол-во
-	 *   price    цена
-	 *
-	 * возвращает структуру без заполненных полей *Executed если ордер еще не исполнился
-	 * в крайнем случае заполняет только ID
-	 */
-	Sell(symbol string, amount float64, price float64) (*MakedOrder, error)
+	GetBalances() (map[string]Balance, error)
 
 	/* Ставит ордер на покупку.
-	 * теперь не вызывает getOrderInfo
 	 *
 	 * arguments:
 	 *   symbol   text-id пары в формате apiclient
-	 * 		(например "BTC_ETH" слева валюта основная, справа валюта которую покупаем при вызове buy)
+	 * 		(например "BTC_ETH" слева валюта в котрой цена, справа валюта в которой кол-во)
 	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
 	 *   amount   кол-во
 	 *   price    цена
@@ -62,49 +56,60 @@ type APIClient interface {
 	 * возвращает структуру без заполненных полей *Executed если ордер еще не исполнился
 	 * в крайнем случае заполняет только ID
 	 */
-	Buy(symbol string, amount float64, price float64) (*MakedOrder, error)
+	Buy(symbol string, amount, price float64) (MakedOrder, error)
+
+	/* Ставит лимитный ордер на продажу.
+	 *
+	 * arguments:
+	 *   symbol   text-id пары в формате apiclient
+	 * 		(например "BTC_ETH" слева валюта в котрой цена, справа валюта в которой кол-во)
+	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
+	 *   amount   кол-во
+	 *   price    цена
+	 *
+	 * возвращает структуру без заполненных полей *Executed если ордер еще не исполнился
+	 * в крайнем случае заполняет только ID
+	 */
+	Sell(symbol string, amount, price float64) (MakedOrder, error)
 
 	/* Отменяет ордер.
 	 *
 	 * arguments:
 	 *   symbol   text-id пары в формате apiclient
-	 * 		(например "BTC_ETH" слева валюта основная, справа валюта которую покупаем при вызове buy)
+	 * 		(например "BTC_ETH" слева валюта в котрой цена, справа валюта в которой кол-во)
 	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
 	 *   id       id ордера в строковом формате биржы
 	 */
-	CancelOrder(symbol string, id string) error
+	CancelOrder(symbol, id string) error
+
+	/* Отменяет все открыте ордера на заданной паре.
+	 *
+	 * arguments:
+	 *   symbol   text-id пары в формате apiclient
+	 * 		(например "BTC_ETH" слева валюта в котрой цена, справа валюта в которой кол-во)
+	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
+	 */
+	CancelAll(symbol string) error
 
 	/* Получает информацию об ордере.
 	 *
 	 * arguments:
 	 *   symbol   text-id пары в формате apiclient
-	 * 		(например "BTC_ETH" слева валюта основная, справа валюта которую покупаем при вызове buy)
+	 * 		(например "BTC_ETH" слева валюта в котрой цена, справа валюта в которой кол-во)
 	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
 	 *   id       id ордера в строковом формате биржы
 	 */
-	GetOrderStatus(id string, symbol string) (*MakedOrder, error)
+	GetOrderStatus(symbol, id string) (MakedOrder, error)
 
-	/* Получает список всех открытых сделок на данном аккаунте, заполняет структуру
-	 * в формате apiclient и возвращает указтель на эту структуру. Вызывается часто,
-	 * следует избегать блокировок.
+	/* Получает список ВСЕХ открытых оредров на данном аккаунте для выбранной пары, заполняет структуру
+	 * в формате apiclient и возвращает эту структуру. Вызывается часто.
 	 *
 	 * arguments:
 	 *   symbol   text-id пары в формате apiclient
-	 * 		(например "BTC_ETH" слева валюта основная, справа валюта которую покупаем при вызове buy)
+	 * 		(например "BTC_ETH" слева валюта в котрой цена, справа валюта в которой кол-во)
 	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
 	 */
-	GetMyOpenOrders(symbol string) (*[]MakedOrder, error)
-
-	/* Получает список всех сделок на данном аккаунте, заполняет структуру
-	 * в формате apiclient и возвращает указтель на эту структуру. Вызывается часто,
-	 * следует избегать блокировок.
-	 *
-	 * arguments:
-	 *   symbol   text-id пары в формате apiclient
-	 * 		(например "BTC_ETH" слева валюта основная, справа валюта которую покупаем при вызове buy)
-	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
-	 */
-	GetMyAllOrders(symbol string) (*[]MakedOrder, error)
+	GetMyOpenOrders(symbol string) ([]MakedOrder, error)
 
 	/* Создает запрос на вывод средств.
 	 *
@@ -119,56 +124,104 @@ type APIClient interface {
 	 */
 	Withdraw(asset string, address string, amount float64) (id string, err error)
 
-	/* Функции доступа к приватным полям реализации. Используются абстрактными
-	 * функциями во внешних модулях (xutils) для упрощения доступа к данным.
-	 * Возвращают соответствующие значения если поле представлено и пустую
-	 * строку если данное поле не используется.
-	 *
-	 * Пример реализации:
-	 *   func (A *XX_API) Prop_Root() string  { return A.root   }
-	 *   func (A *XX_API) Prop_Key()  string  { return A.apiKey }
-	 *   func (A *XX_API) Prop_ID()  string  { return A.apiID }
-	 *   func (A *XX_API) Prop_Logger()  io.Writer  { return A.logger }
-	 *   func (A *XX_API) Prop_Secret() string { return ""       } // нет в XX_API{}
+	// Функции ниже в том числе должны работать, если задать пустые настройки api
+
+	/* Получает список всех возможных text-id пар в формате apiclient на бирже.
+	 * Сортиурет, так что сначала идут USDT пары, затем BTC, затем ETH, после все остальные.
 	 */
-	PropRoot() string
-	PropKey() string
-	PropSecret() string
-	PropID() string
-	PropLogger() io.Writer
+	GetAllSymbols() ([]string, error)
+
+	/* Получает кол-во знаков после запятой для price и amount, заполняет структуру в формате apiclient
+	 *
+	 * arguments:
+	 *   symbol   text-id пары в формате apiclient
+	 * 		(например "BTC_ETH" слева валюта основная, справа валюта которую покупаем при вызове buy)
+	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
+	 */
+	GetDecs(symbol string) (Decimals, error)
+
+	/* Получает биржевой стакан с глубиной, заданной в Init, заполняет структуру в формате apiclient
+	 * и сортирует Asks по возрастанию цены и Bids по убыванию цены.
+	 * Ф-ия вызывается часто, перед каждой сделкой для анализа orderBook.
+	 *
+	 * arguments:
+	 *   symbol   text-id пары в формате apiclient
+	 * 		(например "BTC_ETH" слева валюта в котрой цена, справа валюта в которой кол-во)
+	 * 		в вид который принимает биржа, необходимо привести самому в этой функции
+	 *
+	 * требуется отсортировать Asks по возрастанию цены
+	 * Bids по убыванию цены
+	 */
+	GetOrderBook(symbol string) (OrderBook, error)
+
+	/* Получает цену последней сделки на бирже.
+	 * Ф-ия вызывается часто.
+	 *
+	 * arguments:
+	 *   symbol   text-id пары в формате apiclient
+	 * 		(например "BTC_ETH" слева валюта в котрой цена, справа валюта в которой кол-во)
+	 * 		в вид который принимает биржа, необходимо привести самому в этой функции
+	 */
+	GetLastPrice(symbol string) (float64, error)
+
+	/* Получает 100 последних объемных свеч и свеч цены, заполняет структуру в формате apiclient
+	 * и сортирует по времени (от болле старого к более новому)
+	 *
+	 * arguments:
+	 *   symbol   text-id пары в формате apiclient
+	 * 		(например "BTC_ETH" слева валюта в котрой цена, справа валюта в которой кол-во)
+	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
+	 *   candlePeriod  период свеч в минутах, типа int64
+	 */
+	GetKLine(symbol string, candlePeriod int64) (KLine, error)
+
+	/* Получает 100 последних сделок, заполняет структуру в формате apiclient
+	 * и сортирует ее по времени (от болле старого к более новому)
+	 *
+	 * arguments:
+	 *   symbol   text-id пары в формате apiclient
+	 * 		(например "BTC_ETH" слева валюта основная, справа валюта которую покупаем при вызове buy)
+	 *	 	в вид который принимает биржа, необходимо привести самому в этой функции
+	 */
+	GetTradeHistory(symbol string) ([]Trade, error)
 }
+
+//Side type for enum about order buy or sell types or something like that
+type Side string
 
 //Status type for enum about order status
 type Status string
 
-//Side type for enum about order buy or sell types or something like that
-type Side string
+//Color type for volume candle color
+type Color string
 
 //constants about Status and Side
 const (
 	Buy             Side   = "BUY"
 	Sell            Side   = "SELL"
 	Filled          Status = "FILLED"
-	NotFilled       Status = "NotFilled"
-	PartiallyFilled Status = "PartiallyFilled"
+	NotFilled       Status = "NOT_FILLED"
+	PartiallyFilled Status = "PARTIALLY_FILLED"
+	Red             Color  = "rgba(255, 82, 82, 0.5)"
+	Green           Color  = "rgba(0, 150, 136, 0.5)"
 )
 
 //Balance help struct for APIClient
 type Balance struct {
-	Free   float64 `json:"free"`   //available balance for use in new orders
-	Locked float64 `json:"locked"` //locked balance in orders or withdrawals
+	Free   float64 `json:"free"`   // available balance for use in new orders
+	Locked float64 `json:"locked"` // locked balance in orders or withdrawals
 }
 
 //OrderBook help struct for APIClient
 type OrderBook struct {
-	Asks []Order `json:"asks"` //asks.Price > any bids.Price
+	Asks []Order `json:"asks"` // asks.Price > any bids.Price
 	Bids []Order `json:"bids"`
 }
 
 //Order help struct for APIClient
 type Order struct {
-	Quantity float64 `json:"quantity"`
-	Price    float64 `json:"price"`
+	Amount float64 `json:"amount"`
+	Price  float64 `json:"price"`
 }
 
 //MakedOrder help struct for APIClient
@@ -176,16 +229,49 @@ type MakedOrder struct {
 	ID string `json:"id"`
 	//  Status Should be one of apiclient.Status constants(Filled, NotFilled, PartiallyFilled)
 	Status      Status  `json:"status"`
-	Closed      bool    `json:"closed"`
 	LeftAmount  float64 `json:"leftAmount"`
 	RightAmount float64 `json:"rightAmount"`
 
 	LeftAmountExecuted  float64 `json:"leftAmountExecuted"`
 	RightAmountExecuted float64 `json:"rightAmountExecuted"`
-	//Commission is factically not used
-	Commission   float64 `json:"commission"`
-	Rate         float64 `json:"rate"`
-	RateExecuted float64 `json:"rateExecuted"`
+
+	Commission    float64 `json:"commission"`
+	Price         float64 `json:"price"`
+	PriceExecuted float64 `json:"priceExecuted"` // Real deal price
 	//  Side Should be one of apiclient.Side constants(Buy, Sell)
 	Side Side `json:"side"`
+}
+
+//PriceCandle help struct for APIClient
+type PriceCandle struct {
+	Time  int64   `json:"time"`  // UNIX time in seconds (10 digits)
+	Open  float64 `json:"open"`  // open price for period
+	Close float64 `json:"close"` // close price for period
+	High  float64 `json:"high"`  // high price for period
+	Low   float64 `json:"low"`   // low price for period
+}
+
+//VolumeCandle help struct for APIClient
+type VolumeCandle struct {
+	Time  int64   `json:"time"`  // UNIX time in seconds (10 digits)
+	Value float64 `json:"value"` // volume for period
+	Color Color   `json:"color"` // apiclient.Green if Close > Open, apiclient.Red if Close < Open
+}
+
+//KLine help struct for APIClient
+type KLine struct {
+	PriceCandles  []PriceCandle  `json:"priceCandles"`
+	VolumeCandles []VolumeCandle `json:"volumeCandles"`
+}
+
+type Trade struct {
+	Time   int64   `json:"time"` // UNIX time in seconds (10 digits)
+	Amount float64 `json:"amount"`
+	Price  float64 `json:"price"`
+	Side   Side    `json:"side"` // if side not defined, = Buy if current price > last price, = Sell if current price < last price
+}
+
+type Decimals struct {
+	PriceDecimal  int `json:"priceDecimal"`
+	AmountDecimal int `json:"amountDecimal"`
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gotbitoriginal/apiclient
 
 go 1.12
+
+require gitlab.com/StrRoma/apiclient v0.0.0-20210708181803-3998094ca275
